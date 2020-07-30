491af269fe48a78c5f60d8e8811ca16b708c5366
Carter Wagner <cjwagn1@gmail.com>
2020-07-29 21:45:42 -0500

    Updates

---
+++ precache-manifest.70888634850c551b8930aedb1af8393f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb55380cae5a1b849cf95a61fa2a95fe",
+    "url": "/divine-weather/index.html"
+  },
+  {
+    "revision": "b97d191db4bc19b00690",
+    "url": "/divine-weather/static/css/main.99433cdf.chunk.css"
+  },
+  {
+    "revision": "1f5ef157ee1135c35239",
+    "url": "/divine-weather/static/js/2.bf4d530c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/divine-weather/static/js/2.bf4d530c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b97d191db4bc19b00690",
+    "url": "/divine-weather/static/js/main.bdc197f2.chunk.js"
+  },
+  {
+    "revision": "42be3260603ca9a7d68a",
+    "url": "/divine-weather/static/js/runtime-main.6bd73cc7.js"
+  },
+  {
+    "revision": "df4e324bd2462e2f0ca9cf1558a4ac1f",
+    "url": "/divine-weather/static/media/cold.df4e324b.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/divine-weather/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "13be0af16fedbef552068d6aeffda053",
+    "url": "/divine-weather/static/media/warm.13be0af1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.31a00dc36c98ae8c2b4924a44b8d1f10.js"
+  "/divine-weather/precache-manifest.70888634850c551b8930aedb1af8393f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/divine-weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
